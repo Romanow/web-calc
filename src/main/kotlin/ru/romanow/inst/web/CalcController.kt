1fc1be02f814f65b3bdb13680554d4e05bfe1ac3
Romanow Alex <romanowalex@hotmail.com>
2020-08-31 22:17:42 +0300

    add heroku deploy

--- src/main/kotlin/ru/romanow/inst/web/CalcController.kt
+++ src/main/kotlin/ru/romanow/inst/web/CalcController.kt
@@ -2,6 +2,7 @@ package ru.romanow.inst.web
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -9,14 +10,14 @@ import org.springframework.web.bind.annotation.RestController
 class CalcController {
 
     @GetMapping("/plus")
-    fun plus(a: Int, b: Int): Int = a + b
+    fun plus(@RequestParam a: Int, @RequestParam b: Int): Int = a + b
 
     @GetMapping("/minus")
-    fun minus(a: Int, b: Int): Int = a - b
+    fun minus(@RequestParam a: Int, @RequestParam b: Int): Int = a - b
 
     @GetMapping("/multiply")
-    fun multiply(a: Int, b: Int): Int = a * b
+    fun multiply(@RequestParam a: Int, @RequestParam b: Int): Int = a * b
 
     @GetMapping("/divide")
-    fun divide(a: Int, b: Int): Int = a / b
+    fun divide(@RequestParam a: Int, @RequestParam b: Int): Int = a / b
 }
